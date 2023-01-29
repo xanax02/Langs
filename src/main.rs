b17ba946a139485ce2ca31c452909274a6210402
abhay <abhaytxanax@gmail.com>
2023-01-30 02:45:59 +0530

    user-input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io; // std -> standard library containg io library
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
 }
