bd645218fbb16028dd815d21dd5652f54d9a4309
abhay <abhaytxanax@gmail.com>
2023-02-01 23:02:27 +0530

    vaiables-and-immutability

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,11 @@
 use std::io; // std -> standard library containg io library
-use rand::Rng;
-use std::cmp::Ordering;
 
 fn main() {
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    // println!("your secret_number is {secret_number}");
-
-    loop 
-    {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
     
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-    
-        println!("you guessed {guess}");
-    
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-    
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too Small!"),
-            Ordering::Greater => println!("Too Big!"),
-            Ordering::Equal => {
-                println!("You Win!");
-                break;
-            }
-        } 
-    }
+    let x = 5;
+    println!("the value of x is {x}");
+    x = 6;
+    println!("the value of x is {x}");
 
 }
 
