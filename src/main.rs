6f9e22cac8b89860f0aaa97036c9948fa5178021
abhay <abhaytxanax@gmail.com>
2023-02-01 23:04:22 +0530

    mutable-variables

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,13 @@ fn main() {
     
     let x = 5;
     println!("the value of x is {x}");
-    x = 6;
-    println!("the value of x is {x}");
+    //this will not compile
+    // x = 6;
+    // println!("the value of x is {x}");
 
+    let mut y: u32 =  6;
+    println!("the value of y is {y}");
+    y = 10;
+    println!("the new value of y is {y}");
 }
 
