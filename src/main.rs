2fe37724f72188d2748111a560d7fa81a8baa3c8
abhay <abhaytxanax@gmail.com>
2023-02-12 01:55:48 +0530

    deep-copy-string

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
 fn main() {
-    println!("hello world");
-    let mut str = "abhay";
-    str = "thakur";
-    println!("{}", str);
-    let mut s = String::from("hello");
+    // println!("hello world");
+    // let mut str = "abhay";
+    // str = "thakur";
+    // println!("{}", str);
+    // let mut s = String::from("hello");
 
-    s.push_str(", world!");
-    println!("{}", s);
+    // s.push_str(", world!");
+    // println!("{}", s);
 
-    let mut st: &str = "abhay";
-    let st2: String  = String::from("abhay");
+    // let mut st: &str = "abhay";
+    // let st2: String  = String::from("abhay");
 
-    st = "thakur";
+    // st = "thakur";
+
+    // let s1: String = String::from("hello");
+    // let s2: String = s1;
+    // println!("{}",s1);  //this will not work
 
     let s1: String = String::from("hello");
-    let s2: String = s1;
-    // println!("{}",s1);  this will not work
+    let s2: String = s1.clone();
+
+    println!("s1 = {}, s2 = {}", s1,s2);
 }
