6a55ad4862000f3d32a5eed988c87b4333294f7d
abhay <abhaytxanax@gmail.com>
2023-02-16 21:42:21 +0530

    reference

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+
 fn main() {
+    //-ownerhip-//
+
     // println!("hello world");
     // let mut str = "abhay";
     // str = "thakur";
@@ -17,8 +20,18 @@ fn main() {
     // let s2: String = s1;
     // println!("{}",s1);  //this will not work
 
-    let s1: String = String::from("hello");
-    let s2: String = s1.clone();
+    // let s1: String = String::from("hello");
+    // let s2: String = s1.clone();
 
-    println!("s1 = {}, s2 = {}", s1,s2);
+    // println!("s1 = {}, s2 = {}", s1,s2);
+
+    //-reference-//
+    fn calculate_len(s: &String) -> usize 
+    {
+        return s.len();
+    }
+    let str1: String = String::from("Abhay");
+    let len: usize = calculate_len(&str1);
+    println!("The length of '{}' is {}", str1, len);
+
 }
