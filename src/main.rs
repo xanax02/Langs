aa29aa1f4aa40f9a997ac07330dccb106d878dab
abhay <abhaytxanax@gmail.com>
2023-02-23 16:04:32 +0530

    struct-start

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,19 @@
 
-fn main() {
-    //-ownerhip-//
+fn main() 
+{
+    //-struct-//
 
-    // println!("hello world");
-    // let mut str = "abhay";
-    // str = "thakur";
-    // println!("{}", str);
-    // let mut s = String::from("hello");
+    //program without struct
 
-    // s.push_str(", world!");
-    // println!("{}", s);
-
-    // let mut st: &str = "abhay";
-    // let st2: String  = String::from("abhay");
-
-    // st = "thakur";
-
-    // let s1: String = String::from("hello");
-    // let s2: String = s1;
-    // println!("{}",s1);  //this will not work
-
-    // let s1: String = String::from("hello");
-    // let s2: String = s1.clone();
-
-    // println!("s1 = {}, s2 = {}", s1,s2);
-
-    //-reference-//
-    // fn calculate_len(s: &String) -> usize 
-    // {
-    //     return s.len();
-    // }
-    // let str1: String = String::from("Abhay");
-    // let len: usize = calculate_len(&str1);
-    // println!("The length of '{}' is {}", str1, len);
-
-    // //immutability of reference
-    // // fn change(s: &String) 
-    // // {
-    // //     s.push_str(", World");
-    // // }
-    // let str2: String = String::from("Hello");
-    // // change(&str2);
-    // println!("{}",str2);  // the above code will not compile
-
-    // //mutable References
-    // let mut str3: String = String::from("HELLO");
-    // let r1 = &mut str3;
-    // // let r2: &mut String = &mut str3;  // this will give error as there can only be one mutable references.
-    // println!("{}", r1);
-
-    //String slices
-    // let s: String =  String::from("hello world");
-    // let hello = &s[0..5];
-    // let world: &str = &s[6..11];
-
-    //return first word(with help of string slices)
-    let sentence: String = String::from("Hello WOrld");
-    let word: &str = first_word(&sentence);
-    println!("{}", word);
-
-    fn first_word(s: &String) -> &str
+    fn area(width: i32, height: i32) -> i32
     {
-        let bytes = s.as_bytes();
-        for(i, &item) in bytes.iter().enumerate()
-        {
-            if item == b' '
-            {
-                return &s[0..i];
-            }
-        }
-        return &s[..];
+        width*height
     }
+
+    let width1: i32 = 30;
+    let height1: i32 = 50;
+
+    println!("the area of rectangle is {} square pixels.", area(width1, height1));
+    
 
 }
