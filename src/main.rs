fbde9917ed19e89e6f541a619ebbe5412bca649c
abhay <abhaytxanax@gmail.com>
2023-02-23 16:08:41 +0530

    tuple-refactoring

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,17 @@ fn main()
     let height1: i32 = 50;
 
     println!("the area of rectangle is {} square pixels.", area(width1, height1));
+
+    // refactoring with tuple
+
+    fn area1(dimensions: (u32, u32)) ->u32
+    {
+        dimensions.0 * dimensions.1
+    }
+
+    let rect1: (u32, u32) = (30,50);
+    println!("the area of rectangle with tuple refactoring is {} square pixels", area1(rect1));
+
     
 
 }
