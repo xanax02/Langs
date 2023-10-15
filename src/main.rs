524a0b45c8afd494a78f9a5da28acb124f654aeb
abhay <abhaytxanax@gmail.com>
2023-10-15 19:01:52 +0530

    prj-init

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,16 @@
-use std::io; // std -> standard library containg io library
-use rand::Rng;
-use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-    println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    // println!("your secret_number is {secret_number}");
+    println!("Guess the number");
+    println!("Please input your guess");
 
-    loop 
-    {
-        println!("Please input your guess.");
+    let mut guess: String = String::new();
 
-        let mut guess = String::new();
-    
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-    
-        println!("you guessed {guess}");
-    
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-    
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too Small!"),
-            Ordering::Greater => println!("Too Big!"),
-            Ordering::Equal => {
-                println!("You Win!");
-                break;
-            }
-        } 
-    }
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to get the line");
+
+    println!("You have guessed {guess}");
 
 }
-
