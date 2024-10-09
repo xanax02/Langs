e880e372c7ee732b663bd067db114404a481bc5a
abhay <abhaytxanax@gmail.com>
2024-10-09 21:22:35 +0530

    functions

--- go/go_basics/hello.go
+++ go/go_basics/hello.go
@@ -1,6 +1,29 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func add(x , y int) int {
+	return x+y
+}
+
+func anyNumberResult(x,y string) (string, string) {
+	return y,x
+}
+
+func nakedReturn(sum int) (x, y int) {
+	x = sum*4/9
+	y = sum-x
+	return
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Printf("hwllo %g problems. \n", math.Sqrt(7))
+	fmt.Println(add(42, 13))
+	
+	a,b := anyNumberResult("Hello", "World")
+	fmt.Println(a,b)
+
+	fmt.Println(nakedReturn(69))
 }
