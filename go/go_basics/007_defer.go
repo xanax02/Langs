b947d7df5b237dd55b1d53b9e4ce603825308ec5
abhay <abhaytxanax@gmail.com>
2024-10-22 21:37:57 +0530

    defer

--- go/go_basics/007_defer.go
+++ go/go_basics/007_defer.go
@@ -1,6 +1,38 @@
 package main
-import "fmt"
+import (
+	"os"
+	"fmt"
+)
 
 func main() {
-	
+	defer fmt.Println("world")
+	fmt.Println("Hello")
+
+	f := createFile("./defer-temp.txt")
+	defer closeFile(f)
+	writeFile(f)
+}
+
+func createFile(p string) *os.File {
+	fmt.Println("creating")
+	f, err := os.Create(p)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func writeFile(f *os.File) {
+	fmt.Println("Wrting")
+	fmt.Fprintln(f, "data")
+}
+
+func closeFile(f *os.File) {
+	fmt.Println("Closing")
+	err := f.Close()
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
 }
