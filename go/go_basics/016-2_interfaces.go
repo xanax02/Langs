aa135c01fb774d61a906a518344d76aa89a0f11d
abhay <abhaytxanax@gmail.com>
2024-11-02 22:28:41 +0530

    interfaces

--- go/go_basics/016-2_interfaces.go
+++ go/go_basics/016-2_interfaces.go
@@ -1,5 +1,29 @@
 package main
+import "fmt"
+
+type I interface {
+    M()
+}
+
+type T struct {
+    S string
+}
+
+func(t *T) M() {
+    if t == nil {
+        fmt.Println("<nil>")
+    }
+    fmt.Println(t.S)
+}
+
+func describe(i I) {
+    fmt.Printf("(%v, %T)\n", i,i)
+}
 
 func main() {
+
+    var i I
+
+    var t = *T
 
 }
