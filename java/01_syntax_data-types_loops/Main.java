29c44af4f1d3717ce443c80e6e63c6d5c237f02f
cronix <abhaytxanax@gmail.com>
2025-02-02 23:10:09 +0300

    Number type

--- java/01_syntax_data-types_loops/Main.java
+++ java/01_syntax_data-types_loops/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("HELLO WORLD");
-		final int myNum = 15; //act as a contant
+		final int myNum = 15; //act as a constant
+		
+		float f1 = 35e3f;
+		double d1 = 12e4d;
+
+		System.out.println(f1);
+		System.out.println(d1);
+
+		char ascVal = 65;
+		System.out.println(ascVal);
 	}
 }
