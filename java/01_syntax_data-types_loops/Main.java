212e1f5847fb07cbee1eeda457731b4b8ec6ade7
cronix <abhaytxanax@gmail.com>
2025-02-04 23:54:40 +0300

    switch statement

--- java/01_syntax_data-types_loops/Main.java
+++ java/01_syntax_data-types_loops/Main.java
@@ -25,5 +25,24 @@ public class Main {
 		String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		System.out.println("the length is " + txt.length());
 		System.out.println("to lower case" + txt.toLowerCase());
+	
+		//switch
+		int day = 2;
+		switch(day) {
+			case 1:
+				System.out.println("Monday");
+				break;
+			case 2:
+				System.out.println("Tuesday");
+				break;
+			case 3:
+				System.out.println("Wednesday");
+				break;
+			case 4: 
+				System.out.println("Thursday");
+				break;
+			default:
+				System.out.println("Weekend");
+			}
 	}
 }
