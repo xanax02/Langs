94b27fc02c0cc4b49366b7625eda6fbe6ab2ca3c
cronix <abhaytxanax@gmail.com>
2025-02-05 03:50:49 +0300

    method overloading

--- java/02_methods/Main.java
+++ java/02_methods/Main.java
@@ -7,8 +7,21 @@ public class Main {
 		System.out.println("hello" + fname);
 	}
 
+	//method overloading
+	static int plusMethod(int a, int b) {
+		return a+b;
+	}
+
+	static double plusMethod(double a, double b) {
+		return a+b;
+	}
+
 	public static void main(String[] args) {
 		myMethod();
 		greet("abhay");
+		int sum1 = plusMethod(6,9);
+		double sum2 = plusMethod(6.9, 6.9);
+		System.out.println("integer sum " +  sum1);
+		System.out.println("double sum " + sum2);
 	}
 }
