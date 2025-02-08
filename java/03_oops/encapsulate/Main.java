2f8c91a0355ce7e07d5780fd6bfa6f1009ced7be
cronix <abhaytxanax@gmail.com>
2025-02-08 06:38:31 +0300

    encapsulation

---
+++ java/03_oops/encapsulate/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+	public static void main(String[] args) {
+		Person myObj = new Person();
+		myObj.setName("abhay");
+		String name = myObj.getName();
+		System.out.println(name);
+	}
+}
---
+++ java/03_oops/encapsulate/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+	private String name; //restricted access
+			     //
+	//Getter
+	public String getName() {
+		return name;	
+	}
+
+	//setter 
+	public void setName(String newName) {
+		this.name = newName;
+	}
+}
