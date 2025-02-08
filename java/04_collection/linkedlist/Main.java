adb1d04bfdd8417b1ee604994839c5f6756c8ed4
cronix <abhaytxanax@gmail.com>
2025-02-08 17:45:45 +0300

    Linked list

---
+++ java/04_collection/linkedlist/Main.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+
+public class Main{
+	public static void main(String[] args) {
+		LinkedList<Integer> myLL = new LinkedList<Integer>();
+		myLL.add(1);
+		System.out.println(myLL);
+	}
+}
