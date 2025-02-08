e766e8d1828ba57c093e70b7f15b9c28c4200152
cronix <abhaytxanax@gmail.com>
2025-02-08 07:11:28 +0300

    packages

---
+++ java/03_oops/packages/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter username");
+
+		String username = sc.nextLine();
+		System.out.println("hello" + username);
+	}
+}
