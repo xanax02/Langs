9fb73d1909784137bcd7e153e731281ae7beebf7
abhay <abhaytxanax@gmail.com>
2025-02-19 23:52:11 +0530

    data_types

---
+++ python/01_Basics/data_types.py
@@ -0,0 +1,21 @@
+# Object types/ dataTypes
+
+# Number
+# String
+# List
+# Tuple
+# Dictionary
+# Set
+# Boolean
+# None
+
+username = "Abhay"
+names = ["Abhay", "Thakur"]
+# print(dir(username))
+print(names)
+
+my_dict = {"one": "Abhay", "two": "Thakur"}
+print(my_dict)
+
+my_tuple = (1,2,3)
+print(my_tuple)
