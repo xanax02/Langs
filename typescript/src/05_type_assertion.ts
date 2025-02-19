8c2b84a08b61ff8a41d92a0151cef7545dae529f
abhay <abhaytxanax@gmail.com>
2025-02-19 20:32:59 +0530

    some deletion

--- typescript/src/05_type_assertion.ts
+++
@@ -1,9 +0,0 @@
-const canvas1 = document.getElementById("my-canvas") as HTMLCanvasElement;
-// or
-const canva2 = <HTMLCanvasElement> document.getElementById("my-canvas");
-
-// non-null assertion operator !
-
-function liveDangerously(x? : number | null) {
-    console.log(x!.toFixed());
-}
