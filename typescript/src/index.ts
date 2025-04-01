0e66bd84a0dab3cfc8f31c7f7081bef135db932a
abhay <abhaytxanax@gmail.com>
2025-04-01 18:40:10 +0530

    interfaces

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -10,6 +10,39 @@ function runAfter1Sec(fn: () => void): void {
     }, 1000)
 }
 
-let greee = () => {
-    console.log("HEllo world")
+
+//interfaces
+interface Address {
+  pin: number;
+  city: string;
+  country: string;
+}
+
+interface User {
+    name: string
+    age: number,
+    address?: Address
+}
+
+let user1: User = {
+    name: 'abhay',
+    age: 22,
+    address: {
+        pin: 121212,
+        city: 'Pune',
+        country: 'India'
+    }
+}
+
+let user2: User = {
+    name: "Thakur",
+    age: 22
+}
+
+const isLegal = (user: User): boolean => {
+    return user.age >= 18 ? true : false
+}
+
+interface Office {
+    address: Address
 }
