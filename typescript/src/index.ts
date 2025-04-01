2c0a04f39bf0c536461235b1c600321252bc48be
abhay <abhaytxanax@gmail.com>
2025-04-01 19:07:25 +0530

    interface implementation

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -46,3 +46,53 @@ const isLegal = (user: User): boolean => {
 interface Office {
     address: Address
 }
+
+// Implementing interface
+interface Person {
+    name: string,
+    age: number,
+    // greet: () => string,
+    isLegal(): boolean,
+
+    // greet2(): string
+}
+
+// const person1: Person = {
+//     name: "Abhay",
+//     age: 22,
+//     // greet: () => "HEllo"
+// }
+
+// console.log(person1.greet())
+
+
+// class implementing interface
+class Manager implements Person {
+
+    // name: string;
+    // age: number;
+    // number: string; //can have extra fields
+
+    // constructor(name: string, age: number) {
+    //     this.name = name;
+    //     this.age = age;
+    //     this.number = "1234567890"
+    // }
+
+    constructor(public name: string, public age: number) {} //both means same
+
+    isLegal(): boolean {
+        return this.age > 18
+    }
+
+}
+
+class Ceo extends Manager {
+    constructor() {
+        super("thakur", 25)
+    }
+}
+
+//object of Manger class
+let userManager = new Manager("John", 30)
+console.log("age", userManager.age)
