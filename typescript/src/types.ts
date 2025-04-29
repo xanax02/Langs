a56b40bdf9ad60b8095b73618f9b6c4dd4ab26f2
abhay <abhaytxanax@gmail.com>
2025-04-29 22:04:29 +0530

    types union and intersection

--- typescript/src/types.ts
+++ typescript/src/types.ts
@@ -1,31 +1,37 @@
 type EmployeeType = {
-    name: string
-    startDate: string;
-}
+  name: string;
+  startDate: string;
+};
 
 type ManagerType = {
-    name: string,
-    department: string;
-}
+  name: string;
+  department: string;
+};
 
-type TeamLead = EmployeeType & ManagerType
+//intersection -> have all properites of type
+type TeamLead = EmployeeType & ManagerType;
 
 let t: TeamLead = {
-    name: "abhay:",
-    startDate: "yesterday",
-    department: "CSE"
-}
+  name: "abhay:",
+  startDate: "yesterday",
+  department: "CSE",
+};
 
 //union
 type GoodUser = {
-    name: string;
-    gift: string;
-}
+  name: string;
+  gift: string;
+};
 
-type BadUser ={
-    name: string;
-    ip: string
-}
+type BadUser = {
+  name: string;
+  ip: string;
+};
 
-type UserBehave = GoodUser | BadUser
+// union -> either Gooduser or BadUser or all values of both
+type UserBehave = GoodUser | BadUser;
 
+const union_user: UserBehave = {
+  name: "abhay",
+  gift: "hello hlloo",
+};
