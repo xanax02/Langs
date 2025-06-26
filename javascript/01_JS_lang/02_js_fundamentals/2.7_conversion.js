6665827cc0f764367658dd87e427544362862df7
abhay <ahbaytxanax@gmail.com>
2025-06-26 16:18:52 +0530

    conversions in js

---
+++ javascript/01_JS_lang/02_js_fundamentals/2.7_conversion.js
@@ -0,0 +1,10 @@
+
+//string conversion
+let value = true;
+value = String(value);
+console.log(typeof value);
+
+//numberic conversion
+let str = "1243"
+let numC = Number(str);
+console.log(typeof numC, numC);
